893875f5a86ab2c2421486520581da9fa5ab2bf6
VitzLok <105333945+VitzLok@users.noreply.github.com>
2024-04-03 20:27:52 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+console.log("Oi ta funfando");
+
+const fts = document.getElementById("fts");
+const buy = document.getElementById("buy");
+
+fts.addEventListener("click", function(){
+    window.open("galeria.html", "_blank")
+})
+
+buy.addEventListener("click", function(){
+    window.open("agenda.html", "_blank")
+})
+
